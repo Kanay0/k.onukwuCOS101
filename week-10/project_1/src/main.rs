c5361e8c7ed8fbdeca2dbff39c88cdb78c302912
Kanay0 <kanayochukwu.onukwu@pau.edu.ng>
2023-12-18 14:34:17 +0100

    (My Tenth Commit)

--- week-10/project_1/src/main.rs
+++ week-10/project_1/src/main.rs
@@ -1,3 +1,30 @@
+// Create A Laptop Structure System
+
+struct  Laptop{
+    price:u32,
+    quantity:u32
+}
 fn main() {
-    println!("Hello, world!");
+    //activate the structure
+
+    let laptop1 = Laptop {
+        price:650000,
+        quantity:3
+    };
+    let laptop2 = Laptop{
+        price:755000,
+        quantity:3
+    };
+    let laptop3 = Laptop{
+        price:850000,
+        quantity:3,
+    };
+
+    let hp = laptop1.price * laptop1.quantity;
+    let ibm = laptop2.price * laptop2.quantity;
+    let toshiba = laptop3.price * laptop3.quantity;
+
+    let totals = hp + ibm + toshiba;
+    
+    println!("The Total Of Your Order Is N{}",totals);
 }
